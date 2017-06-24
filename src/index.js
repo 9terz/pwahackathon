2c737fcdce9891ff4afdd6780f0f37388bdc1cf1
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-06-24 15:18:19 +0700

    Change to create-react-app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+import store from './store'
+
+ReactDOM.render(
+  <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
